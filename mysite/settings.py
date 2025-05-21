5968b0ee89c6aeea338d9021b4d5dccd3c0e09e2
alexanderthethricegreatest <117855812+alexanderthethricegreatest@users.noreply.github.com>
2025-05-20 23:50:13 -0400

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -54,3 +55,4 @@ TIME_ZONE = 'UTC'
 USE_I18N = USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
