fe3650889b893ce7422417f322e40251c51c343a
alexanderthethricegreatest <117855812+alexanderthethricegreatest@users.noreply.github.com>
2025-05-20 23:04:19 -0400

    Update urls.py

--- leads/urls.py
+++ leads/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import dashboard
+from .views import dashboard, leads_list
 
 urlpatterns = [
     path("dashboard/", dashboard, name="dashboard"),
+    path("leads/", leads_list, name="leads_list"),
 ]
