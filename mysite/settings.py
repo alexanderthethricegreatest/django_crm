39ea470f423885ce805dda352eb084a555553540
alexanderthethricegreatest <117855812+alexanderthethricegreatest@users.noreply.github.com>
2025-05-21 02:02:33 -0400

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -54,5 +54,6 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = USE_TZ = True
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
